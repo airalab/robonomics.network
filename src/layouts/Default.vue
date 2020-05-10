d3a8a329beba0ccafe6c9566a34910138c485b7f
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2020-05-10 14:27:08 +0400

    index page (technologies added)

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,114 @@
 <template>
   <div class="layout screen">
-    <slot/>
+    <transition name="fade" appear>
+      <slot/>
+    </transition>
+
+    <div class="footer">
+      <strong>#ROS</strong>&nbsp;
+      <strong>#NixOS</strong>&nbsp;
+      <strong>#IPFS</strong>&nbsp;
+      <strong>#Ethereum</strong>&nbsp;
+      <strong>#Polkadot</strong>&nbsp;
+      <br/>
+      <!-- <div class="footer__technologies">
+            <g-image alt="we use ROS" src="~/assets/images/logo-ros.svg" />
+            <g-image alt="we use ROS" src="~/assets/images/logo-nixos.svg" />
+            <g-image alt="we use ROS" src="~/assets/images/logo-ipfs.svg" />
+            <g-image alt="we use ROS" src="~/assets/images/logo-ethereum.svg" />
+            <g-image alt="we use ROS" src="~/assets/images/logo-polkadot.svg" />
+      </div> -->
+      Since 2015
+    </div>
   </div>
 </template>
+
+
+<style lang="scss">
+  .fade-enter-active {
+    transition: opacity .5s;
+  }
+
+  .fade-enter {
+    opacity: 0;
+  }
+
+  .screen{
+    min-height: 100%;
+    position: relative;
+
+    &:before{
+      content: "";
+      position: absolute;
+      top: var(--space);
+      bottom: var(--space);
+      right: var(--space);
+      left: var(--space);
+      border: 1px solid var(--color-dark);
+    }
+
+    @media screen and (max-width: 720px) {
+      &:before{
+        top: calc( var(--space) / 2);
+        bottom: calc( var(--space) / 2);
+        right: calc( var(--space) / 2);
+        left: calc( var(--space) / 2);
+      }
+    }
+  }
+
+  .layout{
+    padding-top: calc( var(--space) * 3 );
+    padding-bottom: 5rem; //padding for footer
+
+    @media screen and (max-width: 720px) {
+      // padding-bottom: 7rem; //padding for footer
+      padding-top: calc( var(--space) * 1.5 );
+    }
+
+    &__content {
+      max-width: var(--content-width);
+      margin-left: auto;
+      margin-right: auto;
+      padding-left: calc( var(--space) * 2);
+      padding-right: calc( var(--space) * 2);
+
+      @media screen and (max-width: 720px) {
+        padding-left: var(--space);
+        padding-right: var(--space);
+      }
+    }
+  }
+
+
+  .footer{
+    position: absolute;
+    bottom: calc(var(--space) + .5rem);
+    left: var(--space);
+    right: var(--space);
+    text-align: center;
+
+
+    // &__technologies{
+
+    //   margin-bottom: .8rem;
+
+    //   transition: .2s all ease-out;
+    //   filter: grayscale(100%);
+
+    //   &:hover{
+    //     filter: grayscale(0);
+    //   }
+
+    //   img{
+    //     display: inline-block;
+    //     vertical-align: middle;
+    //     margin-right: 1.2rem;
+
+    //     @media screen and (max-width: 720px) {
+    //       transform: scale(.8);
+    //     }
+    //   }
+    // }
+  }
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,24 @@
 <template>
   <Layout>
-    <div class="homepage">
-      <g-image :alt="$static.metadata.siteName" src="~/assets/images/robonomics-logo.svg" class="homepage__logo"/>
-      <g-image :alt="$static.metadata.siteName" src="~/assets/images/robonomics-logo.svg" class="homepage__logo_reflect"/>
+    <div class="homepage layout__content" itemscope itemtype="http://schema.org/Product">
+        <g-image itemprop="logo" :alt="$static.metadata.siteName + ' logotype'" src="~/assets/images/robonomics-logo.svg" class="homepage__logo"/>
+      
+        <h1 v-html="$static.metadata.siteName" itemprop="name"/>
+
+        <g-image :alt="$static.metadata.siteName + ' logotype'" src="~/assets/images/robonomics-logo.svg" class="homepage__logo_reflect"/>
+
+        <div class="homepage__description">
+          <p class="hyphens" itemprop="description">During the development of Robonomics Network a lot of experiments happen. Tons of code written and erased after use. It was expected that many Smart City and Industry 4.0 cases could match by not so large count of programming patterns. We decided to generalise it as a set of open-source packages with the name Robonomics Framework.</p>
+
+          <nav class="homepage__menu" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
+            <g-link to="https://github.com/airalab/robonomics/releases" target="_blank" itemprop="url"><span itemprop="name">Releases</span></g-link>
+            <g-link to="https://wiki.robonomics.network" target="_blank" itemprop="url"><span itemprop="name">WIKI</span></g-link>
+            <g-link to="https://dapp.robonomics.network" target="_blank" itemprop="url"><span itemprop="name">DApp</span></g-link>
+            <g-link to="/" itemprop="url"><span itemprop="name">Community</span></g-link>
+          </nav>
+        </div>
     </div>
+
   </Layout>
 </template>
 
@@ -24,19 +39,40 @@ query {
       width: 8rem;
     }
 
-
-    &__logo{
-      margin-top: calc( var(--space) * 3 );
-    }
-
     &__logo_reflect{
       transform-origin: 50% 50%;
       transform: scaleY(-1);
-      position: absolute;
-      top: calc( var(--space) * 8 );
-      left: calc( 50% - 4rem );
+      opacity: .2;
+    }
 
-      opacity: .3;
+    h1{
+      margin: 1rem 0;
+      padding-bottom: 0.3rem;
+      text-shadow: rgba(3, 165, 237, .3) 0 0.3rem;
+    }
+
+    &__description{
+      position: relative;
+      top: -5.5rem;
+    }
+
+    &__menu{
+      display: inline-block;
+      border: 1px solid var(--color-dark);
+      padding: calc(var(--space)/2);
+      margin-top: calc(var(--space)/2);
+
+      box-shadow: .2rem .2rem 0 var(--color-dark);
+
+      a{
+        text-decoration: none;
+        text-transform: uppercase;
+        letter-spacing: 0.05rem;
+        font-weight: 400;
+
+        display: block;
+        margin-bottom: 0.1rem;
+      }
     }
   }
 </style>
