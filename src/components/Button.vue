9e9780f23ae2ca43bfd1cba55d605ff1e9ad8e19
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2020-11-23 13:56:21 +0400

    mobile adaptive, button component

---
+++ src/components/Button.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <g-link :to="link" :class="classes" :style="{transform: 'scale('+scale+')'}">{{label}}</g-link>
+
+</template>
+
+
+
+
+<script>
+
+export default {
+
+  props: {
+
+    link: {
+      type: String,
+      default: false,
+    },
+
+    label: {
+      type: String,
+      default: false,
+    },
+
+    style: {
+      type: String,
+      default: 'border',
+      validator: function (value) {
+        return ['border', 'primary'].indexOf(value) !== -1;
+      }
+    },
+
+    scale: {
+      type: String,
+      default: '1',
+    },
+  },
+
+  computed: {
+    classes() {
+      return {
+        [`button`]: true,
+        [`button__${this.style}`]: true,
+      };
+    },
+  },
+
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
