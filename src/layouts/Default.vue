22f62213f0225d47dc27423d7d8ff41f71a5ead6
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2021-02-02 13:39:30 +0400

    top banner (w school 2021)

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,6 +2,12 @@
   <transition name="fade" appear>
   
     <div class="screen">
+      <a class="banner" href="https://robonomics.network/blog/winter-robonomics-school/">
+        <g-image src="~/assets/images/banners/wschool-video.png" class="hovercontent"/>
+        <span>Robonomics winter school 10 - 24 February 2021</span>
+        <g-image src="~/assets/images/banners/wschool-video.png" class="hovercontent"/>
+      </a>
+
       <header class="header">
         <div class="header-logo">
           <g-link to="/"><g-image :alt="$static.metadata.siteName + ' logotype'" src="~/assets/images/robonomics-logo.svg"/></g-link>
@@ -94,6 +100,57 @@ query {
     color: var(--text-color);
     text-decoration: none;
   }
+
+.screen {
+  .banner {
+    display: block;
+    // position: sticky;
+    // top: 0;
+    // z-index: 100;
+
+    padding: calc(var(--space) * 0.3) var(--screen-padding);
+    background: #590FB7;
+    color: #fff;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    text-decoration: none;
+    text-align: center;
+    font-weight: 500;
+
+    .hovercontent {
+      visibility: hidden;
+      opacity: 0;
+      width: 0;
+    }
+
+    & > * {
+      display: inline-block;
+      vertical-align: middle;
+      vertical-align: -moz-middle-with-baseline;
+      vertical-align: -webkit-baseline-middle;
+    }
+
+    & > *:not(:last-child) {
+      margin-right: calc(var(--space) * 0.5);
+    }
+
+    img {
+      max-height: var(--space);
+    }
+
+    &:hover {
+      color: #fff;
+      background: #6699FF;
+      padding-top: calc(var(--space) * 0.6);
+      padding-bottom: calc(var(--space) * 0.6);
+
+      .hovercontent {
+        animation: FadeIn 0.4s ease-out forwards;
+        width: auto;
+      }
+    }
+  }
+}
 
 </style>
 
