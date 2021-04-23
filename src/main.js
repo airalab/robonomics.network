cdc8b69f8088d347f337f11c63988fc9c1fe3beb
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2021-04-23 10:16:31 +0400

    moved imports to global

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-// Import main css
 import '~/assets/style/index.scss'
-
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import Default from '~/layouts/Default.vue'
+
+import Vue from 'vue'
+
+// directive for animation in view
+import inViewportDirective from 'vue-in-viewport-directive'
+Vue.directive('in-viewport', inViewportDirective)
+
+// for Home page now
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
+
+
 
 export default function (Vue, { router, head, isClient }) {
   head.htmlAttrs = { prefix: 'og: https://ogp.me/ns#', lang: 'en' }
