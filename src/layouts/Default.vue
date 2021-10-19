860d34acadbc1e49933efe1ca4e831ea542bf327
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2021-10-19 13:08:42 +0300

    del crowdloan, add docs in head

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -22,7 +22,8 @@
             <nav class="header-nav sidetext">
                 <g-link to="/intro">{{$ts('Intro')}}</g-link>
                 <g-link to="/vision">Vision</g-link>
-                <g-link to="/kusama-slot">{{$ts('Crowdloan')}}</g-link>
+                <!-- <g-link to="/kusama-slot">{{$ts('Crowdloan')}}</g-link> -->
+                <g-link to="/community/#docs">{{$ts('Docs')}}</g-link>
                 <g-link to="/blog">{{$ts('Blog')}}</g-link>
             </nav>
 
