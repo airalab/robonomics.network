f093d7502cebc85fad837687e61c59af9334850d
A. Bakai @positivecrash <nastin_box@list.ru>
2021-11-18 10:31:20 +0400

    Build issues

--- gridsome.config.js
+++ gridsome.config.js
@@ -81,11 +81,11 @@ module.exports = {
     
   ],
 
-  // chainWebpack: config => {
-  //   const svgRule = config.module.rule('svg')
-  //   svgRule.uses.clear()
-  //   svgRule
-  //     .use('vue-svg-loader')
-  //     .loader('vue-svg-loader')
-  // }
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  }
 };
