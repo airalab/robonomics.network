09c68e78f2c97a7475f03171fefa86dbb8fc9fb9
A. Bakai @positivecrash <nastin_box@list.ru>
2021-11-18 11:43:41 +0400

    Redirect

---
+++ src/pages/redirect.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>This page has moved to a {{ $context.redirect }}. Automatic redirecting</div>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+        return {
+            title: `Redirecting to https://robonomics.network/${this.$context.redirect}`,
+            meta: [
+                {
+                    'http-equiv': 'refresh',
+                    content: `0; url="${this.$context.redirect}"`
+                }
+            ],
+
+            link: () => [
+                {
+                    rel: 'canonical',
+                    href: `https://robonomics.network/${this.$context.redirect}`,
+                    id: 'canonical',
+                },
+            ],
+        }
+  }
+}
+</script>
