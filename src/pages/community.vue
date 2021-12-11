00c2ac3d5f1d0d6c80fdc33384b1206b297cc308
A. Bakai @positivecrash <nastin_box@list.ru>
2021-12-11 22:32:15 +0400

    Community page redirections

---
+++ src/pages/community.vue
@@ -0,0 +1,64 @@
+<template>
+    <layout>
+        <div class="page">
+            <section class="layout layout__content">
+                <h1>This page has moved to other pages</h1>
+                <p>If you are not automaticly redirected please follow links below.</p>
+            </section>
+
+            <section class="layout layout__text">
+                <Card link="/xrt/" image="/website_cover_token.jpg" :imageRound="false" orientation="vertical" imageSize="block">
+                    <h3>{{$ts('Robonomics token')}}, XRT</h3>
+                </Card>
+            </section>
+        </div>
+    </layout>
+</template>
+
+<static-query>
+query {
+  metadata {
+    home
+  }
+}
+</static-query>
+
+<script>
+export default {
+  metaInfo() {
+        return {
+            meta: [
+                {
+                    'http-equiv': 'refresh',
+                    content: `0; url="${this.redirect}"`
+                }
+            ],
+
+            link: () => [
+                {
+                    rel: 'canonical',
+                    href: `${this.$static.metadata.home}${this.redirect}`,
+                    id: 'canonical',
+                },
+            ],
+        }
+  },
+
+  components: {
+      Card: () => import("~/components/TextCard.vue"),
+  },
+
+  computed: {
+      redirect(){
+          if (process.isClient) {
+              if(location.hash === "#token"){
+                return this.$static.metadata.home + '/xrt/'
+            }
+            else {
+                return this.$static.metadata.home
+            }
+          }
+      }
+  }
+}
+</script>
