2cffe36aa07fccc612d217fe5148f11295f0e70b
A. Bakai @positivecrash <nastin_box@list.ru>
2021-12-11 23:41:53 +0400

    Community new pages

---
+++ src/pages/contact.vue
@@ -0,0 +1,33 @@
+<template>
+  <layout>
+
+    <MetaInfo
+      :pageTitle = "'Social media links'"
+    />
+
+    <section class="layout layout__text">
+        <h1>Social media links</h1>
+        <share :assets="contact"/>
+    </section>
+
+  </layout>
+</template>
+
+
+<script>
+
+  import contact from '@/data/contact.yaml'
+
+  export default {
+    components: {
+      MetaInfo: () => import('~/components/MetaInfo.vue'),
+      share: () => import('~/components/Share.vue'),
+    },
+
+    computed: {
+      contact () {
+        return contact
+      }
+    },
+  }
+</script>
