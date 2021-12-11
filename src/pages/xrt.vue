e7e9e7e0ae337529f97d6b9566a19f153353536c
A. Bakai @positivecrash <nastin_box@list.ru>
2021-12-11 23:41:36 +0400

    Community page redirections

--- src/pages/xrt.vue
+++ src/pages/xrt.vue
@@ -9,7 +9,7 @@
 
         <section id="banner" class="section__solid section__blue">
             <div class="layout layout__text">
-                <h1 class="clean">{{$ts('Robonomics token')}}, XRT</h1>
+                <h1>{{$ts('Robonomics token')}}, XRT</h1>
 
                 <section class="text-abstract">
                     {{$ts('XRT is the utility token that will make it possible to control IoT devices on top of the Ethereum network and the Robonomics parachain.')}}
