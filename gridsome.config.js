715d93284f0086529baec147b2de3ca0b8066465
Иван Глебов <glebov.vanya8@gmail.com>
2022-07-06 15:49:46 +0300

    New translations added

--- gridsome.config.js
+++ gridsome.config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       use: "gridsome-plugin-translateit",
       options: {
-        locales: ["en", "ru", "zh", "es", "ko", "de", "ja"],
+        locales: ["en", "ru", "zh", "es", "ko", "de", "ja", "pt", "az"],
         defaultLocale: "en",
         translations: yaml.load(fs.readFileSync('./src/data/locales/translations.yaml', 'utf8')),
         collections: ['blog'],
