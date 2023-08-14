89fa0f17d09e68d25ccfacbb264a524152fac7c7
Julia <uug11@mail.ru>
2023-08-14 17:04:11 +0400

    changed image

--- src/pages/cases/fronius-solar-web.vue
+++ src/pages/cases/fronius-solar-web.vue
@@ -2,7 +2,7 @@
   <CaseLayout
     :path="$route.path"
     introText="Collaboration with real estate developer on Cyprus for tracking the daily distribution, consumption and offsetting any excess carbon footprint."
-    introImg="fronius-solar-web/1.png"
+    introImg="fronius-solar-web/1-new.png"
     link="https://github.com/Multi-Agent-io/solarweb-hass-integration"
     linkText="solarweb-hass-integration"
   >
