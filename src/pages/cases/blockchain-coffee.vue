e85fff43d2026f79fdc161331e8cc228c0bcae18
Julia <uug11@mail.ru>
2023-09-07 18:40:49 +0400

    more tests

--- src/pages/cases/blockchain-coffee.vue
+++ src/pages/cases/blockchain-coffee.vue
@@ -3,10 +3,10 @@
     path="/cases/blockchain-coffee-machine/"
     introText="Collaboration with real estate developer on Cyprus for tracking the daily distribution, consumption and offsetting any excess carbon footprint."
     introImg="blockchain-coffee/1.jpg"
-    link="https://github.com/Multi-Agent-io/robonomics-coffee-maker"
-    linkText="robonomics-coffee-maker"
-    footerTitle="DIY or how to build your own smart blockchain coffee machine"
-    footerText='<p>You can find a detailed tutorial and technical specifications in the open-source GitHub repository. Keep in mind that you can use any coffee machine you prefer and almost any token of your choice to bring this project to life in your own space.</p>'
+    link="https://github.com/Multi-Agent-io/solarweb-hass-integration"
+    linkText="solarweb-hass-integration"
+    footerTitle="The way forward"
+    footerText='<p class="italic-abstract">Reducing energy usage in the office is just as important as compensating for carbon emissions. We can achieve this by making small changes like using energy-efficient lighting and appliances and keeping office equipment well-maintained.</p><p class="italic-abstract">Energy consumption is a big part of our carbon footprint, and businesses must do their part to be more environmentally friendly. Having a system that tracks energy usage and helps offset carbon emissions can greatly reduce the impact of an office building on the environment. By adopting the right approach, businesses can contribute to a greener future.</p>'
   >
   test page
   </CaseLayout>
