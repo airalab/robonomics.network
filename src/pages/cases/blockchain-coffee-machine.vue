13a104241e08cb0ba2f44212ee628dd76d4bc83b
Julia <uug11@mail.ru>
2023-09-07 15:23:47 +0400

    test

--- src/pages/cases/blockchain-coffee-machine.vue
+++ src/pages/cases/blockchain-coffee-machine.vue
@@ -1,24 +1,12 @@
 <template>
-    <CaseLayout
-    :path="$route.path"
-    introImg="blockchain-coffee/1.png"
-    introText="The 'Robonomics Coffee' is a smart coffee machine integrated into the <a href='/'>Robonomics Network</a>. This project aims to illustrate Robonomics' potential in the IoT sphere through a real-world example."
-    withExtras
-    footerTitle="DIY or how to build your own smart blockchain coffee machine"
-    footerText="<p>You can find a detailed tutorial and technical specifications in the open-source GitHub repository. Keep in mind that you can use any coffee machine you prefer and almost any token of your choice to bring this project to life in your own space.</p>"
-    link="https://github.com/Multi-Agent-io/robonomics-coffee-maker"
-    linkText="robonomics-coffee-maker"
-  >
-  test page
-  </CaseLayout>
+  <layout>
+    test page
+  </layout>
 </template>
 
 <script>
 export default {
 
-  components: {
-    CaseLayout: () => import('~/layouts/CaseLayout.vue'),
-  }
 
 }
 </script>
