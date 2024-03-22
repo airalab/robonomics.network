60ed5b7802e862187a843885d2f77aa9bc003a1c
Julia <uug11@mail.ru>
2024-03-22 19:33:14 +0400

    more fixes

--- gridsome.server.js
+++ gridsome.server.js
@@ -40,91 +40,91 @@ module.exports = function (api) {
   })
 
   // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  // api.createManagedPages( ({ createPage }) => {
+  api.createManagedPages( ({ createPage }) => {
 
-  //   // all locales
-  //   const locales =  ["ru", "zh", "es", "ko", "de", "ja", "pt", "az", "it", "tr", "fr", "uk", "ar", "nl"];
+    // all locales
+    const locales =  ["ru", "zh", "es", "ko", "de", "ja", "pt", "az", "it", "tr", "fr", "uk", "ar", "nl"];
 
-  //   createPage(
-  //     {
-  //         path: '/en/',
-  //         component: 'src/pages/redirect.vue',
-  //         context: {
-  //           redirect: '/'
-  //         }
-  //     }
-  //   )
+    createPage(
+      {
+          path: '/en/',
+          component: 'src/pages/redirect.vue',
+          context: {
+            redirect: '/'
+          }
+      }
+    )
 
-  //   createPage(
-  //     {
-  //         path: `/white-paper-2022`,
-  //         component: 'src/pages/redirect.vue',
-  //         context: {
-  //           redirect: '/white-paper'
-  //         }
-  //     }
-  //   )
+    createPage(
+      {
+          path: `/white-paper-2022`,
+          component: 'src/pages/redirect.vue',
+          context: {
+            redirect: '/white-paper'
+          }
+      }
+    )
 
-  //   createPage(
-  //     {
-  //       path: `/shop`,
-  //       component: 'src/pages/redirect.vue',
-  //       context: {
-  //         redirect: '/merch'
-  //       }
-  //     }
-  //   )
+    createPage(
+      {
+        path: `/shop`,
+        component: 'src/pages/redirect.vue',
+        context: {
+          redirect: '/merch'
+        }
+      }
+    )
 
-  //   createPage({
-  //     path: `/robonomics_white_paper_en.pdf/`,
-  //     component: 'src/pages/redirect.vue',
-  //     context: {
-  //       redirect: '/white-paper'
-  //     }
-  //   })
+    createPage({
+      path: `/robonomics_white_paper_en.pdf/`,
+      component: 'src/pages/redirect.vue',
+      context: {
+        redirect: '/white-paper'
+      }
+    })
 
-  //   locales.forEach(l => {
-  //     createPage(
-  //       {
-  //           path: `/${l}/white-paper-2022`,
-  //           component: 'src/pages/redirect.vue',
-  //           context: {
-  //             redirect: '/white-paper'
-  //           }
-  //       }
-  //     )
+    locales.forEach(l => {
+      createPage(
+        {
+            path: `/${l}/white-paper-2022`,
+            component: 'src/pages/redirect.vue',
+            context: {
+              redirect: '/white-paper'
+            }
+        }
+      )
 
-  //     createPage({
-  //       path: `/${l}/shop`,
-  //       component: 'src/pages/redirect.vue',
-  //       context: {
-  //         redirect: '/merch'
-  //       }
-  //     })
-  //   })
+      createPage({
+        path: `/${l}/shop`,
+        component: 'src/pages/redirect.vue',
+        context: {
+          redirect: '/merch'
+        }
+      })
+    })
   
     
-  //     allPossiblePaths.forEach(node => {
+      allPossiblePaths.forEach(node => {
 
-  //       const path = node.path.slice(0, -1).split("/").pop();
+        const path = node.path.slice(0, -1).split("/").pop();
 
-  //       // for blog posts translations
-  //       // translateBlogPost(fs, path)
+        // for blog posts translations
+        // translateBlogPost(fs, path)
   
-  //       // pages for not existing translations
-  //       locales.forEach(locale => {
-  //         if (fs.existsSync(`content/posts/${locale}/${node.name}.md`)) {
-  //           console.log('exists');
-  //         } else {
-  //           createPage({
-  //             path: `/blog/${locale}/${path}`,
-  //             component: './src/templates/BlogTranslations.vue',
-  //           })
-  //         }
-  //       });
+        // pages for not existing translations
+        locales.forEach(locale => {
+          if (fs.existsSync(`content/posts/${locale}/${node.name}.md`)) {
+            console.log('exists');
+          } else {
+            createPage({
+              path: `/blog/${locale}/${path}`,
+              component: './src/templates/BlogTranslations.vue',
+            })
+          }
+        });
 
         
-  //     })
-  // })
+      })
+  })
   
 }
