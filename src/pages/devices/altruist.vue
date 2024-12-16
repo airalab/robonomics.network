509a4ab32c320b5862bcd8ea9fa2c6e2953fdf8b
Julia <uug11@mail.ru>
2024-12-16 16:58:26 +0400

    added new divices pages + translations update

--- src/pages/devices/altruist.vue
+++ src/pages/devices/altruist.vue
@@ -14,8 +14,8 @@
     </DeviceBuySection>
 
     <article class="layout layout__content space-b-2">
-      <p>Join the movement with an open-source hardware sensor integrated into a decentralized map. Share and access noise, dust, and temperature data from other independent sensors nearby. Forget corporate servers—this data is securely distributed worldwide through innovative Web3 technology.</p>
-      <p>Experience the power of true DePIN (Decentralized Physical Infrastructure Network) — a revolutionary technological movement brought to life and thriving for years on our <g-link aria-label="go to decentralized sensor map" to="https://sensors.social/">decentralized sensor map</g-link>.</p>
+      <p>{{ $t('Join the movement with an open-source hardware sensor integrated into a decentralized map. Share and access noise, dust, and temperature data from other independent sensors nearby. Forget corporate servers—this data is securely distributed worldwide through innovative Web3 technology.') }}</p>
+      <p>{{$t('Experience the power of true DePIN (Decentralized Physical Infrastructure Network) — a revolutionary technological movement brought to life and thriving for years on our')}} <g-link aria-label="go to decentralized sensor map" to="https://sensors.social/">{{$t('decentralized sensor map')}}</g-link>.</p>
     </article>
 
     <h2 class="layout title-lined"><span>{{ $t('3 ways to use it') }}</span></h2>
@@ -24,8 +24,8 @@
       <div class="grid-3">
         <section class="oldy">
           <h5 class="space-b-05">{{ $t('Monitor Air Quality') }}</h5>
-          <p>Access <strong>noise, dust, and temperature data</strong> from other independent sensors on our <g-link class="highlight" aria-label="go to decentralized sensor map" to="https://sensors.social/">decentralized sensor map</g-link>.</p>
-          <p>Connect your device as simple as other smart devices and share your environmental outdoor data with community.</p>
+          <p>{{$t('Access')}} <strong>{{$t('noise, dust, and temperature data')}}</strong> {{$t('from other independent sensors on our')}}   <g-link class="highlight" aria-label="go to decentralized sensor map" to="https://sensors.social/">{{$t('decentralized sensor map')}}</g-link>.</p>
+          <p>{{$t('Connect your device as simple as other smart devices and share your environmental outdoor data with community.')}}</p>
           <g-link class="highlight" aria-label="check our firmware" to="https://github.com/airalab/altruist-firmware">{{ $t('Check open-source sensor firmware on GitHub') }}</g-link>
         </section>
 
@@ -34,12 +34,12 @@
            <p>{{ $t("Data processing is always a sensitive issue. That's why we offer a fully open-source") }}
           <g-link class="highlight" to="https://github.com/airalab/sensors-connectivity" aria-label="check open source connectivity module">{{$t('connectivity module')}}</g-link>  
           {{ $t('giving you the freedom to choose which servers to use for processing. No proprietary clouds, no hidden code.') }}</p>
-          <p>Keep in mind, you can join the <g-link class="highlight" aria-label="go to decentralized sensor map" to="https://sensors.social/">global decentralized sensor map</g-link> even with a sensor connected through your own server!</p>
+          <p>{{$t('Keep in mind, you can join the')}} <g-link class="highlight" aria-label="go to decentralized sensor map" to="https://sensors.social/">{{$t('global decentralized sensor map')}}</g-link> {{ $t('even with a sensor connected through your own server!') }}</p>
         </section>
 
         <section class="oldy">
           <h5 class="space-b-05">{{ $t('Run your own Interactive Map') }} ++</h5>
-          <p>Love the idea of a decentralized map but want to customize it with extra layouts and become an administrator of new sovereign sensor infrastructure? You can deploy <g-link to="https://github.com/airalab/sensors.social/" aria-label="GitHub Reposytory for the map" class="highlight">your own version by forking</g-link> our open-source code on GitHub (be sure to check the license type). Remember to use open-source resources responsibly.</p>
+          <p>{{$t('Love the idea of a decentralized map but want to customize it with extra layouts and become an administrator of new sovereign sensor infrastructure? You can deploy')}} <g-link to="https://github.com/airalab/sensors.social/" aria-label="GitHub Repository for the map" class="highlight">{{$t('your own version by forking')}}</g-link> {{$t('our open-source code on GitHub (be sure to check the license type). Remember to use open-source resources responsibly.')}}</p>
         </section>
 
       </div>
@@ -49,13 +49,13 @@
 
     <article class="layout layout__text space-b-2">
       <ul>
-        <li><b>Input Voltage:</b> AC 100-240V 50/60Hz</li>
-        <li><b>Wireless protocol:</b> Supports 2.4GHz Wi-Fi 6 (802.11 b/g/n) and Bluetooth 5 (LE), Zigbee with onboard antenna</li>
-        <li>Equipped with a high-performance <b>32-bit RISC-V processor</b> with clock speed up to 160 MHz, and a low-power 32-bit RISC-V processor with clock speed up to 20MHz</li>
-        <li><b>Memory:</b> 320KB ROM, 512KB of HP SRAM, 16KB LP SRAM and 4MB Flash memory</li>
-        <li><b>USB-C</b> for firmware development and updates</li>
-        <li><b>PM sensor:</b> SDS011 laser PM2.5/PM10 sensor</li>
-        <li><b>Supported sensors:</b> T&H: SHT3X,BME280,HTU21D; Noise Level Sensor:  I2S INMP441; Geiger counter : Radsens.</li>
+        <li><b>{{$t('Input Voltage:')}}</b> {{$t('AC 100-240V 50/60Hz')}}</li>
+        <li><b>{{$t('Wireless protocol:')}}</b> {{$t('Supports 2.4GHz Wi-Fi 6 (802.11 b/g/n) and Bluetooth 5 (LE), Zigbee with onboard antenna')}}</li>
+        <li>{{$t('Equipped with a high-performance')}} <b>{{$t('32-bit RISC-V processor')}}</b> {{$t('with clock speed up to 160 MHz, and a low-power 32-bit RISC-V processor with clock speed up to 20MHz')}}</li>
+        <li><b>{{$t('Memory:')}}</b> {{$t('320KB ROM, 512KB of HP SRAM, 16KB LP SRAM and 4MB Flash memory')}}</li>
+        <li><b>{{$t('USB-C')}}</b> {{$t('for firmware development and updates')}}</li>
+        <li><b>{{$t('PM sensor:')}}</b> {{$t('SDS011 laser PM2.5/PM10 sensor')}}</li>
+        <li><b>{{$t('Supported sensors:')}}</b> {{ $t('T&H: SHT3X,BME280,HTU21D; Noise Level Sensor:  I2S INMP441; Geiger counter : Radsens.') }}</li>
       </ul>
     </article>
 
@@ -66,7 +66,7 @@
         <details>
           <summary class="text-center">{{$t('Steps to set up your Connectivity')}}</summary>
 
-          <p>With Robonomics decentralized map you can use Robonomics Connectivity or create your own Connectivity Service. <b>{{ $t('Robonomics Connectivity') }}</b>: {{$t('Robonomics established three Connectivity hubs to ensure a stable connection. This option switched on by default on the station.')}} <b>{{ $t('Your own Connectivity') }}</b>: {{ $t('You may launch your own hub for receiving data from Altruist Civil Station and further processing.') }}</p>
+          <p>{{ $t('With Robonomics decentralized map you can use Robonomics Connectivity or create your own Connectivity Service.') }} <b>{{ $t('Robonomics Connectivity') }}</b>: {{$t('Robonomics established three Connectivity hubs to ensure a stable connection. This option switched on by default on the station.')}} <b>{{ $t('Your own Connectivity') }}</b>: {{ $t('You may launch your own hub for receiving data from Altruist Civil Station and further processing.') }}</p>
 
           <ol>
             <li>
--- src/pages/devices/riscV.vue
+++ src/pages/devices/riscV.vue
@@ -13,8 +13,8 @@
     </DeviceBuySection>
 
     <article class="layout layout__text space-b-2">
-      <p>Take control of your smart home data with an open-source server and stay connected using Web3 remote control technologies.</p>
-      <p>Experience the power of true DePIN (Decentralized Physical Infrastructure Network) — a revolutionary technological movement brought to life and thriving for years on our <g-link aria-label="go to decentralized sensor map" to="https://sensors.social/">decentralized sensor map</g-link>.</p>
+      <p>{{ $t('Take control of your smart home data with an open-source server and stay connected using Web3 remote control technologies.') }}</p>
+      <p>{{ $t('Experience the power of true DePIN (Decentralized Physical Infrastructure Network) — a revolutionary technological movement brought to life and thriving for years on our') }} <g-link aria-label="go to decentralized sensor map" to="https://sensors.social/">{{ $t('decentralized sensor map') }}</g-link>.</p>
     </article>
 
     <article class="layout space-b-2">
@@ -22,22 +22,22 @@
 
       <div class="grid-3">
         <section class="oldy">
-          <h5 class="space-b-05">Worldwide avalaible</h5>
-          <p>There are no restrictions on using this smart home server anywhere in the world.</p>
+          <h5 class="space-b-05">{{ $t('Worldwide available') }}</h5>
+          <p>{{ $t('There are no restrictions on using this smart home server anywhere in the world.') }}</p>
         </section>
         <section class="oldy">
-          <h5 class="space-b-05">Free from vendor lock-in</h5>
-          <p>Available over 3,000 integrations with devices from a wide range of manufacturers.</p>
+          <h5 class="space-b-05">{{ $t('Free from vendor lock-in') }}</h5>
+          <p>{{ $t('Available over 3,000 integrations with devices from a wide range of manufacturers.') }}</p>
         </section>
         <section class="oldy">
-          <h5 class="space-b-05">Secure Web3 Cloud</h5>
-          <p>No hidden data logs on corporate servers while still allowing remote control.</p>
+          <h5 class="space-b-05">{{ $t('Secure Web3 Cloud') }}</h5>
+          <p>{{ $t('No hidden data logs on corporate servers while still allowing remote control.') }}</p>
         </section>
       </div>
     </article>
 
 
-    <h2 class="layout title-lined"><span>Well-engineered hardware architecture</span></h2>
+    <h2 class="layout title-lined"><span>{{ $t('Well-engineered hardware architecture') }}</span></h2>
 
     <article class="layout layout__text space-b-2">
       <p>{{ $t('RISC-V is an open and modular processor architecture based on a reduced instruction set (RISC). Unlike proprietary solutions, it offers flexibility for customization without licensing restrictions, making it ideal for IoT devices and servers. RISC-V enables highly efficient and cost-effective designs, which translate into lower power consumption and better performance of your home server.') }}</p>
@@ -52,23 +52,23 @@
         </ul>
     </article>
 
-    <h2 class="layout title-lined"><span>Broad device range with open-source software</span></h2>
+    <h2 class="layout title-lined"><span>{{ $t('Broad device range with open-source software') }}</span></h2>
 
     <article class="layout layout__text space-b-2">
-      <p>By installing an open-source platform Home Assistant on this server, you can create a smart home with a wide variety of devices from different manufacturers, without being tied to a specific brand.</p>
-      <p>With extensive integrations for Zigbee and Wi-Fi, Home Assistant connects nearly 4,000 devices, from temperature sensors to energy monitors, right out of the box using Zigbee2MQTT.</p>
+      <p>{{ $t('By installing an open-source platform Home Assistant on this server, you can create a smart home with a wide variety of devices from different manufacturers, without being tied to a specific brand.') }}</p>
+      <p>{{$t('With extensive integrations for Zigbee and Wi-Fi, Home Assistant connects nearly 4,000 devices, from temperature sensors to energy monitors, right out of the box using Zigbee2MQTT.')}}</p>
     </article>
 
-    <h2 class="layout title-lined"><span>Truly decentralized cloud for quick remote control</span></h2>
+    <h2 class="layout title-lined"><span>{{ $t('Truly decentralized cloud for quick remote control') }}</span></h2>
     
     <article class="layout layout__text space-b-2">
-      <p>With over 10 years of expertise in decentralized technologies, we deliver exceptional peer-to-peer communication for your smart home. Enjoy remote device access via parachain or Libp2p, cryptographic user access control, and secure storage of backups and digital twins of your smart home in an encrypted decentralized cloud.</p>
+      <p>{{ $t('With over 10 years of expertise in decentralized technologies, we deliver exceptional peer-to-peer communication for your smart home. Enjoy remote device access via parachain or Libp2p, cryptographic user access control, and secure storage of backups and digital twins of your smart home in an encrypted decentralized cloud.') }}</p>
     </article>
 
-    <h2 class="layout title-lined"><span>Help & support</span></h2>
+    <h2 class="layout title-lined"><span>{{ $t('Help & support') }}</span></h2>
     <article class="layout layout__text space-b-2">
       <ul>
-        <li><g-link class="highlight" to="https://wiki.robonomics.network/docs/home-server-on-risc/#ubuntu-installation">Home server on RISC V</g-link></li>
+        <li><g-link class="highlight" to="https://wiki.robonomics.network/docs/home-server-on-risc/#ubuntu-installation">{{ $t('Home server on RISC V') }}</g-link></li>
         <li><g-link class="highlight" to="https://wiki.robonomics.network/docs/home-server-on-risc/#home-assistant-core-installation">{{$t('Home Assistant Core Installation')}}</g-link></li>
         <li><g-link class="highlight" to="https://wiki.robonomics.network/docs/home-server-on-risc/#robonomics-integration-installation">{{$t('Robonomics Integration Installation')}}</g-link></li>
         <li><g-link class="highlight" to="https://wiki.robonomics.network/docs/zigbee-devices/">{{ $t('Zigbee Devices in Zigbee2MQTT') }}</g-link></li>
